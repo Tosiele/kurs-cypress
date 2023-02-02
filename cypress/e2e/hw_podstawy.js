d154e886b19f779ea85252ad13db4de59eab20f4
Tosiele <tosiadulak@gmail.com>
2023-02-02 19:57:50 +0100

    Feat: Wrote the firs version of hw code

--- cypress/e2e/hw_podstawy.js
+++ cypress/e2e/hw_podstawy.js
@@ -1,10 +1,13 @@
 describe('home work for basics', () => {
     it('making a screenshot', function() {
         
-        
+        cy.visit('https://fabrykatestow.pl/');
+        cy.get('span').contains('POKAŻ CZEGO SIĘ NAUCZĘ').eq(0).click({force: true});
+        cy.scrollTo('.elementor-background-overlay');
+        cy.screenshot();
 
 
     })
 })
 
-//<a href="https://fabrykatestow.pl/ciekawostki/">Newsletter</a>
+//<div class="elementor-background-overlay"></div>
