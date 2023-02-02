6b24fc48cc968b43358acfcbda1688e012b9e20c
Tosiele <tosiadulak@gmail.com>
2023-02-02 19:22:26 +0100

    Docs: Making a file for the first home work

---
+++ cypress/e2e/hw_podstawy.js
@@ -0,0 +1,10 @@
+describe('home work for basics', () => {
+    it('making a screenshot', function() {
+        
+        
+
+
+    })
+})
+
+//<a href="https://fabrykatestow.pl/ciekawostki/">Newsletter</a>
